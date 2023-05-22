35fa52a3f11887a747684c8bc29f7ac7c8abcfb6
= <=>
2023-05-21 22:56:59 -0500

    commit aft class 9

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { Children, lazy, Suspense } from "react";
 import ReactDOM  from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { createBrowserRouter , RouterProvider, Outlet} from "react-router-dom";
+import ResturantMenu from "./components/ResturantMenu";
+import Shimmer from "./components/Shimmer";
+
+const Instamart = lazy(() => import("./components/Instamart"));
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
         </div>
-    )
-}
+    );
+};
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/resturant/:id",
+                element: <ResturantMenu />,
+            },
+            {
+                path: "/instamart",
+                element: <Suspense fallback={<Shimmer />    }>
+                    <Instamart />
+                </Suspense>
+            }
+            
+        ]
+    },
+    {
+       
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+
+root.render(<RouterProvider router={appRouter} />);
